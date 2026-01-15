616caddc59db8bd6a25a8c0b118ad350484b44d6
Naadbramha <tanvi.ambike@naadbramha.com>
2026-01-15 18:55:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Show image popup
-function showMessage() {
-    document.getElementById("imagePopup").style.display = "flex";
-}
-
-// Close popup
-function closeImage() {
-    document.getElementById("imagePopup").style.display = "none";
-}
